4f2f3d7c548023f578cf4040bc65a06e77a57dab
Young Xu <1397190480@qq.com>
2021-02-13 23:16:31 +0800

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -628,19 +628,24 @@
         </b-modal>
         <!-- 登陆方式模态 -->
         <b-modal id="modal-login" centered title="登陆方式" hide-footer>
-            <b-button variant="info" class="mr-2" @click="loginBy('github')"><i class="fab fa-github fa-lg pr-1"></i>Github
+            <b-button variant="info" size="lg" class="mr-2" @click="loginBy('qq')"><img src='../assets/qq_new.png'
+                                                                              class="icon-size-std pr-1" alt="">QQ登录
             </b-button>
-            <b-button variant="info" class="mr-2" @click="loginBy('qq')"><img src='../assets/qq.png'
-                                                                              class="icon-size-tpl pr-1"></img>QQ
+
+            <b-button variant="info" size="lg" class="mr-2" @click="loginBy('github')"><img src='../assets/github.svg'
+                                                                                            class="icon-size-std pr-1" alt="">Github
             </b-button>
+
+<!--            <b-button variant="info" size="lg" class="mr-2" @click="loginBy('github')"><i class="fab fa-github fa-lg pr-1"></i>Github-->
+<!--            </b-button>-->
             <!--            <b-button variant="info" class="mr-2" @click="loginBy('gitee')"><img src='../assets/gitee.svg'-->
             <!--                                                                                 class="icon-size-tpl pr-1" alt="">Gitee-->
             <!--            </b-button>-->
             <!--            <b-button variant="info" class="mr-2" @click="loginBy('osc')"><img src='../assets/oschina.svg'-->
             <!--                                                                               class="icon-size-osc pr-1" alt="">OSC-->
             <!--            </b-button>-->
-            <b-button variant="info" class="mr-2" @click="loginBy('dingtalk')"><img src='../assets/dingtalk.svg'
-                                                                                    class="icon-size-osc pr-1" alt="">钉钉
+            <b-button variant="info" size="lg" class="mr-2" @click="loginBy('dingtalk')"><img src='../assets/dingtalk.svg'
+                                                                                    class="icon-size-std pr-1" alt="">钉钉
             </b-button>
         </b-modal>
         <b-modal id="modal-bindWxPusher" centered hide-header ok-title="确定" cancel-title="取消" @ok="bindWxPusher">
@@ -1448,6 +1453,9 @@ export default {
             } else if (method === "dingtalk") {
                 localStorage.setItem("loginType", "dingTalk");
                 window.location.href = this.dingTalk;
+            } else if (method === "qq") {
+                localStorage.setItem("loginType", "qq");
+                window.location.href = this.qq;
             }
             // else if (method === "gitee") {
             //     localStorage.setItem("loginType", "gitee");
@@ -1593,12 +1601,17 @@ export default {
                 });
         }
 
-        // code 检测
+        // code 检测 || access_token 检测
         let code = this.$route.query.code;
+        let accessToken = this.$route.query.access_token;
         if (code !== undefined) {
             //推送到专门页面处理
             this.$router.push({name: "Callback", query: {code: code}});
+        } else if (accessToken !== undefined) {
+            //推送到专门页面处理
+            this.$router.push({name: "Callback", query: {code: accessToken}});
         }
+
         // 登录出错
         let error = this.$route.query.error;
         if (error !== undefined) {
@@ -1696,15 +1709,9 @@ export default {
     background-color: #007bff !important;
 }
 
-.icon-size-tpl {
-    width: 22px;
-    height: 22px;
-}
-
-.icon-size-osc {
-    width: 25px;
-    height: 25px;
-    vertical-align: bottom;
+.icon-size-std {
+    /*width: 22px;*/
+    height: 30px;
 }
 
 .main-body {
