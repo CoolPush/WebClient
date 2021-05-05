fc404ed3544fdf552fdf49a28674dec3ce987b91
xuthus5 <xuthus5@gmail.com>
2021-05-05 14:43:22 +0800

    fix

--- src/views/Deploy.vue
+++ src/views/Deploy.vue
@@ -15,7 +15,7 @@
                         v2_Arm64
                     </b-button>
                 </div>
-                <p class="mt-4">说明: 机器人修改自 <a href="https://github.com/Mrs4s/go-cqhttp">go-cqhttp</a> , 已魔改机器人可自动通过好友申请
+                <p class="mt-4">说明: 机器人修改自 <a href="https://github.com/Mrs4s/go-cqhttp">go-cqhttp</a> , 已修改机器人可自动通过好友申请
                     , 自动通过入群申请</p>
 
                 <p>如何使用: 下载本程序,第一次执行时,会生成配置文件,修改配置文件,填写好QQ号码和密码,配置HTTP请求的参数.<br/>运行后,<strong class="text-danger">防火墙/服务器安全组</strong>放行配置好的端口,默认是5700
@@ -23,6 +23,8 @@
                 <p>更详细的教程可以参考 <a
                     href="https://docs.go-cqhttp.org/guide/quick_start.html#%E5%9F%BA%E7%A1%80%E6%95%99%E7%A8%8B">go-cqhttp</a>
                     使用教程.</p>
+
+                <p>QQ交流群: <a href="https://shang.qq.com/wpa/qunwpa?idkey=2c22cb324dc36e260043185618cbe8763ed63dd5a985ee5e181e20ba2390e78a">824187964</a></p>
             </div>
 
             <b-button variant="primary" class="mt-4">
